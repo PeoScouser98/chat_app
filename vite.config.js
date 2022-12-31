4cddf98955da76db5fb829dcdd589dccf3dcee5f
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-01 03:11:01 +0700

    feat: :fire: chat feature using socket

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import alias from "@rollup/plugin-alias";
 
-// https://vitejs.dev/config/
+import path, { resolve } from "path";
+const rootDir = resolve(__dirname);
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		alias({
+			entries: [
+				{
+					find: "@",
+					replacement: resolve(rootDir, "src"),
+				},
+			],
+		}),
+	],
+	resolve: {
+		alias: {
+			"@/*": path.resolve(__dirname, "./src/"),
+		},
+	},
+
 	server: {
 		port: 3000,
 		hmr: { overlay: false },
