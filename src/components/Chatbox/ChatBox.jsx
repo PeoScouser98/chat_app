e4c3315860166b8adcb8a95780921a69aa869038
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-06 02:17:01 +0700

    fix show messages

--- src/components/Chatbox/ChatBox.jsx
+++ src/components/Chatbox/ChatBox.jsx
@@ -8,13 +8,14 @@ const ChatBox = () => {
 	const { chatsList } = useContext(ChatContext);
 
 	useEffect(() => {
+		setMessages(currentChat.messages);
 		socket.on("receive_message", (newMessage) => {
 			setMessages((prev) => [...prev, newMessage]);
 		});
 		return () => {
 			socket.off("receive_message");
 		};
-	}, [currentChat, chatsList]);
+	}, [currentChat, chatsList, socket]);
 
 	return (
 		<div className="flex flex-1 flex-col gap-4 p-4 overflow-y-scroll scroll">
